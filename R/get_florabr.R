79ec848f6daeeb33b16d53202877d02e7fce8aa1
Weverton <wevertonf1993@hotmail.com>
2023-08-21 23:01:05 -0500

    Package commited

---
+++ R/get_florabr.R
@@ -0,0 +1,97 @@
+#' Download the latest version of Brazilian Flora 2020 database
+#'
+#' @description
+#' This function downloads the latest or an older version of Brazilian Flora 2020 database, merges the information into a single data.frame, and saves this data.frame in the specified directory.
+#'
+#'
+#' @param output_dir (character) a directory to save the data downloaded from Brazilian Flora 2020
+#' @param data_version (character) Version of the Brazilian Flora database to download. Use "latest" to get the most recent version, updated weekly. Alternatively, specify an older version (e.g., data_version = "393.319"). Default value is "latest".
+#' @param overwrite (logical) If TRUE, data is overwritten. Default = TRUE.
+#'
+#' @returns
+#' The function downloads the latest version of the Brazilian Flora 2020 database from the official source. It then merges the information into a single data.frame, containing details on species, taxonomy, occurrence, and other relevant data.
+#' The merged data.frame is then saved as a file in the specified output directory. The data is saved in a format that allows easy loading using the \code{\link{load_florabr}} function for further analysis in R.
+#' @usage get_florabr(output_dir, data_version = "latest", overwrite = T)
+#' @export
+#'
+#' @importFrom httr GET write_disk
+#' @importFrom XML htmlParse xpathSApply xmlGetAttr
+#' @importFrom utils unzip
+#' @importFrom utils read.csv
+#' @references
+#' Brazilian Flora 2020. Jardim Botânico do Rio de Janeiro. Available at:
+#' http://floradobrasil.jbrj.gov.br/
+#' @examples
+#' \dontrun{
+#' dir.create("brazilianflora") #Create a directory to save data
+#' my_dir <- "brazilianflora" #Set directory to save data
+#' #Download, merge and save data
+#' get_florabr(output_dir = my_dir, data_version = "latest", overwrite = TRUE)
+#' }
+get_florabr <- function(output_dir = NULL, data_version = "latest", overwrite = T) {
+  #Set folder
+  if(is.null(output_dir)) {
+    stop(paste("Argument output_dir is not defined, this is necessary for",
+         "\n downloading and saving data"))
+  }
+  if (!is.character(output_dir)) {
+    stop(paste0("Argument output_dir must be a character, not ", class(output_dir)))
+  } else {
+    path_data <- output_dir
+  }
+
+  if (!is.character(data_version)) {
+    stop(paste0("Argument data_version must be a character, not ", class(data_version)))
+  }
+
+  if (!is.logical(overwrite)) {
+    stop(paste0("Argument overwrite must be logical, not ", class(overwrite)))
+  }
+
+
+  #Print message
+  cat(paste("Data will be saved in", path_data, "\n"))
+
+
+  if(data_version != "latest") {
+  link_download <- paste0("https://ipt.jbrj.gov.br/jbrj/archive.do?r=lista_especies_flora_brasil&v=",
+                          data_version)
+  }
+
+
+  if(data_version == "latest") {
+  #Get link of latest version
+  response <- httr::GET("https://ipt.jbrj.gov.br/jbrj/resource?r=lista_especies_flora_brasil")
+  parse <- XML::htmlParse(response)
+  links <- unlist(XML::xpathSApply(parse, path = "//a", XML::xmlGetAttr, "href"))
+  download_pattern <- "https://ipt.jbrj.gov.br/jbrj/archive.do?r=lista_especies_flora_brasil&v="
+  link_download <- subset(links, grepl(download_pattern, links, fixed = T))
+  }
+
+  #Get version
+  version_data <- gsub(".*lista_especies_flora_brasil&v=([0-9.]+).*", "\\1",
+                       link_download)
+
+  #Print message
+  if(!is.null(version_data)) {
+      cat(paste("Downloading version:", version_data, "\n"))
+
+
+  #Download data
+  httr::GET(link_download, httr::write_disk(file.path(path_data, paste0(version_data, ".zip")),
+                                      overwrite = overwrite))
+  }
+
+  #Unzip folder
+  utils::unzip(zipfile = paste0(file.path(path_data, version_data), ".zip"),
+        exdir = file.path(path_data, version_data))
+
+  cat("Merging data. Please wait a moment...\n")
+  #Merge data
+  merge_data(path_data = path_data, version_data = version_data)
+
+  #Print final message
+  cat(paste("Data downloaded and merged successfully. Final data saved in",
+              file.path(path_data, version_data, "CompleteBrazilianFlora.rds")))
+
+}
---
+++ R/get_spat_occ.R
@@ -0,0 +1,224 @@
+#' Get Spatial polygons (SpatVectors) of species based on its distribution (States and Biomes) according do Brazilian Flora 2020
+#'
+#' @param data (data.frame) the data.frame imported with the
+#' \code{\link{load_florabr}} function.
+#' @param species (character) one or more species names (only genus and specific epithet, eg. "Araucaria angustifolia")
+#' @param State (logical) get SpatVector of states with occurrence of the species? Default = TRUE
+#' @param Biome (logical) get SpatVector of biomes with occurrence of the species? Default = TRUE
+#' @param intersection (character) get a Spatvector representing the intersection between States and Biomes with occurrence of the specie? To use intersection = TRUE, you must define State = TRUE and Biome = TRUE". Default = TRUE
+#' @param State_vect (SpatVector) a SpatVector of the Brazilian states. By default, it uses the SpatVector provided by geobr::read_state(). It can be another Spatvector, but the structure must be identical to geobr::read_state().
+#' @param state_column (character) name of the column in State_vect containing state abbreviations. Only use if Biome_vect is not null.
+#' @param Biome_vect (SpatVector) a SpatVector of the Brazilian biomes. By default, it uses the SpatVector provided by geobr::read_biomes(). It can be another SpatVector, but the structure must be identical to geobr::read_biomes().
+#' @param biome_column (character) name of the column in Biome_vect containing names of brazilian biomes (in English: "Amazon", "Atlantic_Forest", "Caatinga", "Cerrado", "Pampa" and "Pantanal". Only use if Biome_vect is not null.
+#' @param verbose (logical) Whether to display species being filtered during function execution. Set to TRUE to enable display, or FALSE to run silently. Default = TRUE.
+#'
+#' @return A list with SpatVectors of States and/or Biomes and/or Intersections for each specie.
+#' @importFrom terra subset unwrap intersect mask
+#' @importFrom data.table rbindlist
+#' @export
+#' @references
+#' Brazilian Flora 2020. Jardim Botânico do Rio de Janeiro. Available at:
+#' http://floradobrasil.jbrj.gov.br/
+#' @examples
+#' library(terra)
+#' data("bf_data") #Load Brazilian Flora data
+#' spp <- c("Araucaria angustifolia", "Adesmia paranensis") #Example species
+#' #Get states, biomes and intersection states-biomes of species
+#' spp_spt <- get_spat_occ(data = bf_data, species = spp, State = TRUE,
+#'                        Biome = TRUE, intersection = TRUE, State_vect = NULL,
+#'                        Biome_vect = NULL, verbose = TRUE)
+#'
+#'
+#' #Plot states of occurrence of Araucaria angustifolia
+#' plot(spp_spt[[1]]$States, main = names(spp_spt)[[1]])
+#' #Plot biomes of occurrence of Araucaria angustifolia
+#' plot(spp_spt[[2]]$Biomes, main = names(spp_spt)[[2]])
+#' #Plot intersection between states and biomes of occurrence of
+#' #Araucaria angustifolia
+#' plot(spp_spt[[1]]$States_Biomes)
+#'
+get_spat_occ <- function(data = NULL, species = NULL, State = TRUE,
+                         Biome = TRUE,
+                         intersection = TRUE,
+                         State_vect = NULL, state_column = NULL,
+                         Biome_vect = NULL, biome_column = NULL,
+                         verbose = TRUE) {
+  if (missing(data)) {
+    stop("Argument data is not defined")
+  }
+
+  if (missing(species)) {
+    stop("Argument occ is not defined")
+  }
+
+  if (!inherits(data, "data.frame")) {
+    stop(paste0("Argument data must be a data.frame, not ", class(data)))
+  }
+
+  if (!is.character(species)) {
+    stop(paste0("Argument species must be a character, not ", class(species)))
+  }
+
+  if (!is.logical(State)) {
+    stop(paste0("Argument State must be logical, not ", class(State)))
+  }
+
+  if (!is.logical(Biome)) {
+    stop(paste0("Argument Biome must be logical, not ", class(Biome)))
+  }
+
+  if (!is.logical(intersection)) {
+    stop(paste0("Argument intersection must be logical, not ", class(intersection)))
+  }
+
+  if (!is.null(State_vect) && !inherits(State_vect, "SpatVector")) {
+    stop(paste0("Argument State_vect must be NULL or a SpatVector, not ",
+                class(State_vect)))
+  }
+
+  if (!is.null(Biome_vect) && !inherits(Biome_vect, "SpatVector")) {
+    stop(paste0("Argument Biome_vect must be NULL or a SpatVector, not ",
+                class(Biome_vect)))
+  }
+
+  if (!is.null(State_vect) && !is.character(state_column)) {
+    stop(paste0("Argument state_column must be a character, not ", class(state_column)))
+  }
+
+  if (!is.null(Biome_vect) && !is.character(biome_column)) {
+    stop(paste0("Argument biome_column must be a character, not ", class(biome_column)))
+  }
+
+  if (!is.logical(verbose)) {
+    stop(paste0("Argument verbose must be logical, not ", class(verbose)))
+  }
+
+  #Check colnames in data
+  if(!all(c("species", "States", "Biome") %in%
+          colnames(data))) {
+    stop("Important columns are missing in data. Check if data is an object created by 'load_florabr()")
+  }
+
+  #Check if there is at least one TRUE in states or biomes
+  if(State == FALSE & Biome == FALSE){
+    stop("At least one of the parameters State or Biome must be true")
+  }
+  if(intersection == TRUE & (State == FALSE | Biome == FALSE)) {
+    stop("To use intersection = TRUE, you must define State = TRUE and Biome = TRUE")
+  }
+
+  #Load data
+  d <- data[,c("species", "States", "Biome")]
+
+  #Check if all species are in Brazilin Flora data
+  spp <- get_binomial(species_names = species)
+  #Get binomial names of species
+  spp_out <- setdiff(spp, unique(data$species))
+  if(length(spp_out) > 0) {
+    stop(paste(length(spp_out), "species are not in the data. Check the species names using the check_names() function"))
+  }
+  #Subset info
+  d_info <- subset(d, d$species %in% spp)
+  d_info[d_info == ""] <- NA
+
+  #Get only one line by species, merging information of same species
+  sp_info <- lapply(seq_along(spp), function(i) {
+    sp <- subset(d_info, d_info$species == spp[i])
+    sp$States <- paste0(na.omit(unique(sp$States)),
+                        collapse = ";")
+    sp$Biome <- paste0(na.omit(unique(sp$Biome)),
+                       collapse = ";")
+    return(sp)
+  })
+  sp_info <- unique(data.table::rbindlist(sp_info))
+
+  #Load data
+  if(State == TRUE) {
+  states <- terra::unwrap(florabr::states)
+  }
+  if(Biome == TRUE) {
+    biomes <- terra::unwrap(florabr::biomes)
+  }
+
+  #Check personal vectors (if provided)
+  #States
+  if(!is.null(State_vect)){
+    names(State_vect)[which(names(State_vect) == state_column)] <- "abbrev_state"
+    check_matches <- setdiff(states$abbrev_state, State_vect$abbrev_state)
+    if(length(check_matches) > 0) {
+      stop(paste0("Invalid States in ", state_column,
+                  "\nCheck the structure of the Spatvector provided in State_vect"))
+    } else {
+      states <- State_vect
+    }}
+  #Biomes
+  if(!is.null(Biome_vect)){
+    names(Biome_vect)[which(names(Biome_vect) == biome_column)] <- "name_biome"
+    check_matches <- setdiff(biomes$name_biome, Biome_vect$name_biome)
+    if(length(check_matches) > 0) {
+      stop(paste0("Invalid Biomes in ", biome_column,
+                  "\nCheck the structure of the Spatvector provided in Biome_vect"))
+    } else {
+      biomes <- Biome_vect
+    }}
+
+  #Get state
+  l_occ <- lapply(seq_along(spp), function(i){
+    occ_i <- subset(sp_info, sp_info$species == spp[i])
+
+    if(State == FALSE) {states_v <- NULL}
+
+    if(State == TRUE) {
+      if(isTRUE(verbose)) {
+        cat(paste("Getting states of", spp[i], "\n")) }
+
+      sp_i_state <- unique(gsub(";", "|", occ_i$States[1]))
+
+      if(sp_i_state == "" | is.na(sp_i_state)) {
+        cat(paste(spp[i], "lacks info about State - SpatialVector not returned"))
+        states_v <- "No_info"
+      } else {
+        states_v <- terra::subset(states, grepl(sp_i_state,
+                                                    states$abbrev_state)) }
+    }
+
+    if(Biome == FALSE) {biomes_v <- NULL}
+
+    if(Biome == TRUE) {
+      if(isTRUE(verbose)) {
+        cat(paste("Getting biomes of", spp[i], "\n")) }
+      sp_i_biome<- unique(gsub(";", "|", occ_i$Biome[1]))
+
+      if(sp_i_biome == "" | is.na(sp_i_biome)) {
+        cat(paste(spp[i], "lacks info about Biome - SpatialVector not returned"))
+        biomes_v <- "No_info"
+      } else {
+        biomes_v <- terra::subset(biomes, grepl(sp_i_biome,
+                                                    biomes$name_biome)) }
+    }
+
+    if(intersection == FALSE) {int_v <- NULL}
+
+    if(intersection == TRUE) {
+      if(isTRUE(verbose)) {
+        cat(paste("Getting biomes of", spp[i], "\n")) }
+      if((sp_i_biome == "" | is.na(sp_i_biome)) & verbose == TRUE) {
+        cat(paste(spp[i], "lacks info about states - Impossible to get intersection with states"))
+      }
+      if((sp_i_biome == "" | is.na(sp_i_biome)) & verbose == TRUE) {
+        cat(paste(spp[i], "lacks info about biomes - Impossible to get intersection with biomes"))
+      }
+
+
+    int_v <- terra::intersect(biomes_v, states_v) }
+
+    #Save objects in a list
+    final_list <- list(states_v, biomes_v, int_v)
+    names(final_list) <- c("States", "Biomes", "States_Biomes")
+    return(final_list)
+  })
+  names(l_occ) <- spp
+  #Drop off null elements
+  l_occ <- lapply(l_occ, function(x) x[lengths(x) > 0])
+  return(l_occ)
+} #End if function
