e0990596f3883ad0e4bff8fee38067044251ccbf
wevertonbio <wevertonf1993@hotmail.com>
2024-03-19 15:20:25 -0300

    Increase coverage of codecov for check_version and select_species

--- tests/testthat/test-check_version.R
+++ tests/testthat/test-check_version.R
@@ -3,5 +3,10 @@ test_that("Check_version warning that there isn't any version available", {
   my_dir <- file.path(tempdir(), "Random_folder")
   dir.create(my_dir)
   expect_message(check_version(my_dir))
+
+  #It does not work if data_dir is missing or an object with invalid class
+  expect_error(check_version())
+  expect_error(check_version(data_dir = TRUE))
+
 })
 #unlink(my_dir, recursive = T, force = T)
