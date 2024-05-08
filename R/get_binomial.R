52f109bbdc871b2eb13e8f106681eec5a209dd27
wevertonbio <wevertonf1993@hotmail.com>
2024-05-08 20:22:03 -0300

    Remove whitespece between words and leading or trailing whitespace

--- R/get_binomial.R
+++ R/get_binomial.R
@@ -5,6 +5,9 @@
 #'
 #' @return A vector with the binomial names (Genus + specific epithet).
 #' @usage get_binomial(species_names)
+#' @references
+#' Brazilian Flora 2020. Jardim Botânico do Rio de Janeiro. Available at:
+#' http://floradobrasil.jbrj.gov.br/
 #' @export
 #'
 #' @examples
@@ -19,6 +22,13 @@ get_binomial <- function(species_names) {
     stop(paste0("Argument species_names must be a character, not ",
                 class(species_names)))
   }
+
+  # Remove excess of whitespace between words
+  species_names <- gsub("\\s+", " ", species_names)
+
+  # Remove leading and/or trailing whitespace
+  species_names <- trimws(species_names)
+
 
   selected_species_names <- vapply(species_names, FUN.VALUE = character(1),
                                    function(text) {
